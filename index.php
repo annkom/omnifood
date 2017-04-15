2ff7c53e71d1e0b3b7ccf1defe9d0645afaea540
annkom <anna@x8.io>
2017-04-15 10:52:51 +0100

    fixed few bugs: - changed url for redirecting after sending message - changed email address to which message is sent - fixed typo in index.php - fixed whitespace missing in styles.css

--- index.php
+++ index.php
@@ -332,12 +332,12 @@
                         <div class="row">
                             
                             <?php 
-                            if($_GET['successs'] == 1) {
+                            if($_GET['success'] == 1) {
                                 echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
                             }
                             
                             if($_GET['success'] == -1) {
-                                echo "div class=\"form-essages error\">Oops! Something went wrong. Please try again!</div>";
+                                echo "<div class=\"form-messages error\">Oops! Something went wrong. Please try again!</div>";
                             }
                             ?>
                         </div>    
--- mailer.php
+++ mailer.php
@@ -8,12 +8,12 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=-1#form");
+        header("Location: https://omnifood-test.000webhostapp.com/index.php?success=-1#form");
         exit;
     }
 
     // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "hello@webdesigncourse.co";
+    $recipient = "ania.komoniecka+omnifood@gmail.com";
 
     // Set the email subject.
     $subject = "New contact from $name";
@@ -30,6 +30,6 @@
     mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=1#form");
+    header("Location: https://omnifood-test.000webhostapp.com/index.php?success=1#form");
 
 ?>
